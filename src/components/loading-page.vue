f5deac757019789968d523bb450db93f92f4e315
m1ns2o <srjms0128@gmail.com>
2023-10-12 17:34:18 +0900

    6cut photo layout

---
+++ src/components/loading-page.vue
@@ -0,0 +1,169 @@
+<script setup lang="ts">
+</script>
+<template>
+		<div class="main">
+                <div class="loading-container">
+                    <div class="loading"></div>
+                    <div id="loading-text">loading</div>
+                </div>
+			<!-- <h1>서비스 명</h1> -->
+			<div class="slot">
+                <slot></slot>
+            </div>
+		</div>
+</template>
+
+<style scoped>
+.main {
+    padding-top: 10%;
+	height: 100%;
+	text-align: center;
+    .slot{
+        margin-top: 5%;
+    }
+	/* background-image: linear-gradient(to right, #fc5c7d, #6a82fb); */
+}
+
+
+h1 {
+	padding-top: 20%;
+	padding-bottom: 10%;
+	font-size: 7em;
+}
+
+h3 {
+	font-size: 3em;
+}
+/* 
+#link {color: #E45635;display:block;font: 12px "Helvetica Neue", Helvetica, Arial, sans-serif;text-align:center; text-decoration: none;}
+#link:hover {color: #CCCCCC}
+
+#link, #link:hover {-webkit-transition: color 0.5s ease-out;-moz-transition: color 0.5s ease-out;-ms-transition: color 0.5s ease-out;-o-transition: color 0.5s ease-out;transition: color 0.5s ease-out;} */
+
+/** BEGIN CSS **/
+        
+@keyframes rotate-loading {
+    0%  {transform: rotate(0deg);-ms-transform: rotate(0deg); -webkit-transform: rotate(0deg); -o-transform: rotate(0deg); -moz-transform: rotate(0deg);}
+    100% {transform: rotate(360deg);-ms-transform: rotate(360deg); -webkit-transform: rotate(360deg); -o-transform: rotate(360deg); -moz-transform: rotate(360deg);}
+}
+
+@-moz-keyframes rotate-loading {
+    0%  {transform: rotate(0deg);-ms-transform: rotate(0deg); -webkit-transform: rotate(0deg); -o-transform: rotate(0deg); -moz-transform: rotate(0deg);}
+    100% {transform: rotate(360deg);-ms-transform: rotate(360deg); -webkit-transform: rotate(360deg); -o-transform: rotate(360deg); -moz-transform: rotate(360deg);}
+}
+
+@-webkit-keyframes rotate-loading {
+    0%  {transform: rotate(0deg);-ms-transform: rotate(0deg); -webkit-transform: rotate(0deg); -o-transform: rotate(0deg); -moz-transform: rotate(0deg);}
+    100% {transform: rotate(360deg);-ms-transform: rotate(360deg); -webkit-transform: rotate(360deg); -o-transform: rotate(360deg); -moz-transform: rotate(360deg);}
+}
+
+@-o-keyframes rotate-loading {
+    0%  {transform: rotate(0deg);-ms-transform: rotate(0deg); -webkit-transform: rotate(0deg); -o-transform: rotate(0deg); -moz-transform: rotate(0deg);}
+    100% {transform: rotate(360deg);-ms-transform: rotate(360deg); -webkit-transform: rotate(360deg); -o-transform: rotate(360deg); -moz-transform: rotate(360deg);}
+}
+
+@keyframes rotate-loading {
+    0%  {transform: rotate(0deg);-ms-transform: rotate(0deg); -webkit-transform: rotate(0deg); -o-transform: rotate(0deg); -moz-transform: rotate(0deg);}
+    100% {transform: rotate(360deg);-ms-transform: rotate(360deg); -webkit-transform: rotate(360deg); -o-transform: rotate(360deg); -moz-transform: rotate(360deg);}
+}
+
+@-moz-keyframes rotate-loading {
+    0%  {transform: rotate(0deg);-ms-transform: rotate(0deg); -webkit-transform: rotate(0deg); -o-transform: rotate(0deg); -moz-transform: rotate(0deg);}
+    100% {transform: rotate(360deg);-ms-transform: rotate(360deg); -webkit-transform: rotate(360deg); -o-transform: rotate(360deg); -moz-transform: rotate(360deg);}
+}
+
+@-webkit-keyframes rotate-loading {
+    0%  {transform: rotate(0deg);-ms-transform: rotate(0deg); -webkit-transform: rotate(0deg); -o-transform: rotate(0deg); -moz-transform: rotate(0deg);}
+    100% {transform: rotate(360deg);-ms-transform: rotate(360deg); -webkit-transform: rotate(360deg); -o-transform: rotate(360deg); -moz-transform: rotate(360deg);}
+}
+
+@-o-keyframes rotate-loading {
+    0%  {transform: rotate(0deg);-ms-transform: rotate(0deg); -webkit-transform: rotate(0deg); -o-transform: rotate(0deg); -moz-transform: rotate(0deg);}
+    100% {transform: rotate(360deg);-ms-transform: rotate(360deg); -webkit-transform: rotate(360deg); -o-transform: rotate(360deg); -moz-transform: rotate(360deg);}
+}
+
+@keyframes loading-text-opacity {
+    0%  {opacity: 0}
+    20% {opacity: 0}
+    50% {opacity: 1}
+    100%{opacity: 0}
+}
+
+@-moz-keyframes loading-text-opacity {
+    0%  {opacity: 0}
+    20% {opacity: 0}
+    50% {opacity: 1}
+    100%{opacity: 0}
+}
+
+@-webkit-keyframes loading-text-opacity {
+    0%  {opacity: 0}
+    20% {opacity: 0}
+    50% {opacity: 1}
+    100%{opacity: 0}
+}
+
+@-o-keyframes loading-text-opacity {
+    0%  {opacity: 0}
+    20% {opacity: 0}
+    50% {opacity: 1}
+    100%{opacity: 0}
+}
+.loading-container,
+.loading {
+    height: 200px;
+    position: relative;
+    width: 200px;
+    border-radius: 100%;
+}
+
+
+.loading-container { margin: 40px auto }
+
+.loading {
+    border: 10px solid transparent;
+    /* 로딩 바 두께 */
+    border-color: transparent var(--black) transparent var(--black);
+    -moz-animation: rotate-loading 1.5s linear 0s infinite normal;
+    -moz-transform-origin: 50% 50%;
+    -o-animation: rotate-loading 1.5s linear 0s infinite normal;
+    -o-transform-origin: 50% 50%;
+    -webkit-animation: rotate-loading 1.5s linear 0s infinite normal;
+    -webkit-transform-origin: 50% 50%;
+    animation: rotate-loading 1.5s linear 0s infinite normal;
+    transform-origin: 50% 50%;
+}
+
+/* .loading-container:hover .loading {
+    border-color: transparent #E45635 transparent #E45635;
+}
+.loading-container:hover .loading, */
+.loading-container .loading {
+    -webkit-transition: all 0.5s ease-in-out;
+    -moz-transition: all 0.5s ease-in-out;
+    -ms-transition: all 0.5s ease-in-out;
+    -o-transition: all 0.5s ease-in-out;
+    transition: all 0.5s ease-in-out;
+}
+
+#loading-text {
+    -moz-animation: loading-text-opacity 2s linear 0s infinite normal;
+    -o-animation: loading-text-opacity 2s linear 0s infinite normal;
+    -webkit-animation: loading-text-opacity 2s linear 0s infinite normal;
+    animation: loading-text-opacity 2s linear 0s infinite normal;
+    color: var(--black);
+    font-family: "Helvetica Neue, "Helvetica", ""arial";
+    font-size: 10px;
+    font-weight: bold;
+    margin-top: 95px;
+    opacity: 0;
+    position: absolute;
+    text-align: center;
+    text-transform: uppercase;
+    top: 0;
+    width: 200px; 
+    /* fwidth값 == 로딩 원 값
+    fontsize + marintop*2 = widthd */
+}
+
+</style>
--- src/views/PiniaTest.vue
+++ src/views/PiniaTest.vue
@@ -1,1 +1,133 @@
-<template></template>
+<script lang="ts">
+//import { ref, onMounted } from 'vue'
+//import Vue from "vue";
+import * as html2canvas from 'html2canvas'
+import Layout6 from '@/components/Layout-Frame6.vue'
+import Layout4 from '@/components/Layout-Frame4.vue'
+import card from '@/components/Card-View.vue'
+// import { useCounterStore } from '@/stores/index'
+
+// const store = useCounterStore()
+//html2canvas scale옵션 찾아보기 -> 이미지 배율&해상도 옵션
+
+export default {
+  components: {
+    Layout6,
+    Layout4,
+    card
+  },
+  methods: {
+    downloadImg(): void {
+      html2canvas.default(this.$refs.layout).then((canvas) => {
+        const link = document.createElement('a')
+        link.download = 'filename.jpg'
+        link.href = canvas.toDataURL()
+        document.body.appendChild(link)
+        link.click()
+      })
+    }
+  },
+  data() {
+    return {
+      layoutWidth: '160px',
+      layoutHeight: '240px',
+      footer: 'none',
+      borderwidth: '5px'
+    }
+  }
+}
+</script>
+
+<template>
+  <!-- <div class="layout" ref="layout">
+    <div class="layout__images">
+      <div class="layout__images__box"></div>
+      <div class="layout__images__box"></div>
+      <div class="layout__images__box"></div>
+      <div class="layout__images__box"></div>
+      <div class="layout__images__box"></div>
+      <div class="layout__images__box"></div>
+    </div>
+    <div class="layout__footer">
+      <div class="left">Hello World</div>
+      <div class="right">
+        <div class="qr"></div>
+      </div>
+    </div>
+  </div> -->
+  <!-- <Layout6cut /> -->
+  
+  <Layout4
+    />
+
+  <button type="button" @click="downloadImg">download</button>
+  <button type="button" @click="increament">++</button>
+  <Layout6 qr="adss" />
+  <card>
+    <Layout6
+      :layoutWidth="layoutWidth"
+      :layoutHeight="layoutHeight"
+      :footer="footer"
+      :borderwidth="borderwidth"
+    />
+    <h2>\5000</h2>
+  </card>
+  <font-awesome-icon :icon="['fas', 'caret-right']" />
+  <v-icon icon="home" />
+</template>
+<!-- <style lang="scss" scoped>
+.layout {
+  display: flex;
+  flex-direction: column;
+
+  overflow: hidden;
+
+  background-color: #333333;
+  width: 4in;
+  height: 6in;
+  .layout__images {
+    width: 100%;
+    flex: 1;
+
+    display: grid;
+    grid-template-rows: repeat(3, 1fr);
+    grid-template-columns: repeat(2, 1fr);
+
+    gap: 10px;
+    padding: 10px;
+    padding-bottom: 0;
+
+    .layout__images__box {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+
+      background-color: #eeeeee;
+      overflow: hidden;
+    }
+  }
+
+  .layout__footer {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    background-color: #333333;
+    color: white;
+
+    padding: 10px;
+
+    .qr {
+      background-color: red;
+      width: 20px;
+      height: 20px;
+    }
+  }
+}
+.big {
+  width: 400px;
+  height: 400px;
+  background-color: red;
+  opacity: 0.1;
+}
+</style> -->
+<style lang="scss" scoped></style>
